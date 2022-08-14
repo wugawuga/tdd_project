079c3b7454cba9a450b1620a6cd5a11ea3a92dac
Wugawuga <dnr3655@naver.com>
2022-08-14 17:03:44 +0900

    feat - Game class - 컴퓨터 난수와 유저정답과 비교 기능 - 볼, 스트라이크 구분 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,33 @@
-package baseball;public class Game {
+package baseball;
+
+public class Game {
+
+    GameStatus status = new GameStatus();
+
+    public Game(int[] answer, int[] userAnswer) {
+        compareAnswerToUserAnswer(answer, userAnswer);
+    }
+
+    private void compareAnswerToUserAnswer(int[] answer, int[] userAnswer) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                checkMatch(answer[i], userAnswer[j], i, j);
+            }
+        }
+    }
+
+    private void checkMatch(int computer, int user, int i, int j) {
+        if (user == computer) {
+            if (i == j) {
+                status.strike();
+            } else {
+                status.ball();
+            }
+        }
+    }
+
+    public String gameResult() {
+        return this.status.toString();
+    }
+
 }
