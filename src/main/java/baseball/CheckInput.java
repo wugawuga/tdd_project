d56eb54f1e6860c79f72eea37ad4de974a51ecb4
Wugawuga <dnr3655@naver.com>
2022-08-14 17:01:32 +0900

    feat - CheckInput class - 유저가 입력한 정답 예외처리 - 유저가 입력한 정답 컴퓨터 난수랑 비교하기 위해 가공

--- src/main/java/baseball/CheckInput.java
+++ src/main/java/baseball/CheckInput.java
@@ -1,2 +1,39 @@
-package baseball;public class CheckInput {
+package baseball;
+
+public class CheckInput {
+
+    private final int NUMBEROFANSWER = 3;
+
+    private int[] answerArray;
+
+    public CheckInput(String playerAnswer) {
+        int convertToInteger = convertPlayerAnswerToInteger(playerAnswer);
+        this.answerArray = convertPlayerAnswerToArray(convertToInteger);
+    }
+
+    public int[] convertPlayerAnswerToArray(int playerAnswer) {
+        int[] changeArray = new int[NUMBEROFANSWER];
+
+        for (int i = NUMBEROFANSWER - 1; i >= 0; i--) {
+            changeArray[i] = playerAnswer % 10;
+            playerAnswer /= 10;
+        }
+
+        return changeArray;
+    }
+
+    public int convertPlayerAnswerToInteger(String playerAnswer) {
+        if (playerAnswer.length() != NUMBEROFANSWER) {
+            throw new IllegalArgumentException(NUMBEROFANSWER + "자리를 숫자를 입력하세요");
+        }
+        try {
+            return Integer.parseInt(playerAnswer);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBEROFANSWER + "자리를 숫자를 입력하세요");
+        }
+    }
+
+    public int[] finishCheck() {
+        return answerArray;
+    }
 }
