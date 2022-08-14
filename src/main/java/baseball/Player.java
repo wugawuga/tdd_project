b3a996a99940eaf04c9534ac9931f32ee56b4ad7
Wugawuga <dnr3655@naver.com>
2022-08-14 17:00:47 +0900

    feat - Player class - 유저 정답 입력기능

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,2 +1,11 @@
-package baseball;public class Player {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+    public String writeAnswer() {
+        String userAnswer = Console.readLine();
+        return userAnswer;
+    }
 }
