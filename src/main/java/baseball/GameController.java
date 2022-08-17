3211635eea2559ad923f6708201b3756957291ff
JungWuk Yun <dnr3655@naver.com>
2022-08-17 23:29:15 +0900

    refactor - GameController class - Printer 분리 - result.equals("3스트라이크") 를 메소드로 쪼개기

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,43 +1,40 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class GameController {
 
-    boolean flag = true;
+    private boolean flag = true;
+    private final Player player = new Player(); // 유저 클래스 -> 정답 입력
+    private final Printer printer = new Printer();
+    private final Computer computer = new Computer(); // 컴퓨터 클래스 -> 정답 생성
 
     public void start() {
-        Computer computer = new Computer(); // 컴퓨터 클래스 -> 정답 생성
         int[] answer = computer.makeAnswer();
         playGameByUser(answer);
     }
 
     private void playGameByUser(int[] answer) {
         while (flag) {
-            Player player = new Player(); // 유저 클래스 -> 정답 입력
-            System.out.print("정답을 입력하세요 : ");
+            printer.inputAnswer();
             String playerAnswer = player.writeAnswer();
 
             CheckInput check = new CheckInput(playerAnswer); // 체크인풋 클래스 -> 유저입력 체크
             int[] playerAnswerToArray = check.finishCheck(); // 유저 정답 -> 배열
             Game playGame = new Game(answer, playerAnswerToArray);
-            String result = playGame.gameResult();
-            System.out.println(result);
-            if (result.equals("3스트라이크")) {
+            printer.printResult(playGame.gameResult());
+            if (playGame.win()) {
+                printer.restartEnd();
                 againGame();
             }
         }
     }
 
     private void againGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String goStop = Console.readLine();
+        String goStop = player.writeAnswer();
         if (goStop.equals("1")) {
             start();
         }
         if (goStop.equals("2")) {
-            System.out.println("게임 종료");
+            printer.end();
             flag = false;
         }
     }
