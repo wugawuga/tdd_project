226b0e5aebd4f403a4e2db2f57a06adc3687aa76
JungWuk Yun <dnr3655@naver.com>
2022-09-12 22:36:48 +0900

    feat(baseball): 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- src/main/java/baseballLecture/Application.java
+++ src/main/java/baseballLecture/Application.java
@@ -1,5 +1,9 @@
 package baseballLecture;
 
+import java.util.List;
+
+import baseballLecture.domain.NumberGenerator;
+
 /**
  * 객체 지향 프로그래밍
  *	1. 기능을 가지고 있는 클래스를 인스턴스(객체)화 한다
@@ -9,9 +13,8 @@ package baseballLecture;
 
 public class Application {
 	public static void main(String[] args) {
-		// 객체 지향 프로그래밍
-		// 1. 기능을 가지고 있는 클래스를 인스턴스(객체)화 한다
-		// 2. 필요한 기능을 각 인스턴스가 수행하게 한다
-		// 3. 각 결과를 합친다
+		final NumberGenerator generator = new NumberGenerator();
+		final List<Integer> numbers = generator.createRandomNumbers();
+		System.out.println(numbers);
 	}
 }
