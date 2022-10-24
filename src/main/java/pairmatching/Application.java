498e9351f086bf8c9428ece9f0da4d90b93d40b1
wugawuga <dnr3655@naver.com>
2022-10-24 15:13:04 +0900

    style: import 제거, print 제거

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,20 +1,12 @@
 package pairmatching;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Application {
 	public static void main(String[] args) {
-		//TODO: 구현 진행
 
 		List<String> backendCrews = FileReader.read("backend-crew.md");
 		List<String> frontedCrews = FileReader.read("frontend-crew.md");
 
-		System.out.println("backendCrews = " + backendCrews);
-		System.out.println("frontedCrews = " + frontedCrews);
 	}
 }
