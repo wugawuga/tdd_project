1f3041718ab75038bd1a92092df79d98248d0f7d
wugawuga <dnr3655@naver.com>
2022-10-24 14:55:09 +0900

    feat: 파일 읽기 기능

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,37 @@
 package pairmatching;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
 	public static void main(String[] args) {
 		//TODO: 구현 진행
+		try {
+			InputStream inputStream1 = Application.class
+				.getClassLoader()
+				.getResource("backend-crew.md")
+				.openStream();
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(inputStream1));
+			List<String> backendCrew = br1.lines().collect(Collectors.toList());
+			System.out.println("backendCrew = " + backendCrew);
+		} catch (Exception e) {
+			System.out.println("파일 이름을 확인해주세요");
+		}
+
+		try {
+			InputStream inputStream1 = Application.class
+				.getClassLoader()
+				.getResource("backend-crew.md")
+				.openStream();
+			BufferedReader br1 = new BufferedReader(new InputStreamReader(inputStream1));
+			List<String> backendCrew = br1.lines().collect(Collectors.toList());
+			System.out.println("backendCrew = " + backendCrew);
+		} catch (Exception e) {
+			System.out.println("파일 이름을 확인해주세요");
+		}
 	}
 }
