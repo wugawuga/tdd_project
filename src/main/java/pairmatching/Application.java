a54ca8f3575bd0ad0f86c4e9608b4db63000a3ef
wugawuga <dnr3655@naver.com>
2022-10-24 15:09:32 +0900

    refactor: 파일 읽기 기능 FileReader 클래스로 역할 분리

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -10,24 +10,11 @@ import java.util.stream.Collectors;
 public class Application {
 	public static void main(String[] args) {
 		//TODO: 구현 진행
-		List<String> backendCrew = crewList("backend-crew.md");
-		System.out.println(backendCrew);
-		List<String> frontendCrew = crewList("frontend-crew.md");
-		System.out.println(frontendCrew);
-	}
 
-	private static List<String> crewList(String fileName) {
-		try {
-			InputStream inputStream1 = Application.class
-				.getClassLoader()
-				.getResource(fileName)
-				.openStream();
-			BufferedReader br1 = new BufferedReader(new InputStreamReader(inputStream1));
-			List<String> crewList = br1.lines().collect(Collectors.toList());
-			return crewList;
-		} catch (Exception e) {
-			System.out.println("파일 이름(" + fileName + ")을 확인해주세요");
-			return null;
-		}
+		List<String> backendCrews = FileReader.read("backend-crew.md");
+		List<String> frontedCrews = FileReader.read("frontend-crew.md");
+
+		System.out.println("backendCrews = " + backendCrews);
+		System.out.println("frontedCrews = " + frontedCrews);
 	}
 }
--- src/main/java/pairmatching/FileReader.java
+++ src/main/java/pairmatching/FileReader.java
@@ -1,2 +1,25 @@
-package pairmatching;public class FileReader {
+package pairmatching;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileReader {
+
+	public static List<String> read(String fileName) {
+		try {
+			InputStream inputStream = Application.class
+				.getClassLoader()
+				.getResource(fileName)
+				.openStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+			List<String> allLines = br.lines().collect(Collectors.toList());
+			return allLines;
+		} catch (Exception e) {
+			System.out.println("파일 이름(" + fileName + ")을 확인해주세요");
+			return null;
+		}
+	}
 }
