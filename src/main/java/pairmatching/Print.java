13859a8574edb5f837feb2705029dcc29a9778e3
wugawuga <dnr3655@naver.com>
2022-10-24 16:41:13 +0900

    feat: 출력 클래스 분리

--- src/main/java/pairmatching/Print.java
+++ src/main/java/pairmatching/Print.java
@@ -1,2 +1,13 @@
-package pairmatching;public class Print {
+package pairmatching;
+
+import static pairmatching.Options.*;
+
+public class Print {
+	public static void options() {
+		System.out.println("기능을 선택하세요.");
+		System.out.println(MATCHING +". 페어 매칭");
+		System.out.println(SELECT +". 페어 조회");
+		System.out.println(RESET +". 페어 초기화");
+		System.out.println(QUIT + ". 종료");
+	}
 }
