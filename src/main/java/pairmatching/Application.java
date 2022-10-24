e3425db344dbadbd6baa8ebc68967ac6df0a4152
wugawuga <dnr3655@naver.com>
2022-10-24 16:41:48 +0900

    refactor: matching 클래스 분리

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,12 +1,42 @@
 package pairmatching;
 
+import static pairmatching.Options.*;
+
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+	private static final List<String> backendCrews = FileReader.read("backend-crew.md");
+	private static final List<String> frontedCrews = FileReader.read("frontend-crew.md");
+
 	public static void main(String[] args) {
+		Matching matching = new Matching(backendCrews, frontedCrews);
+		boolean programFlag = true;
 
-		List<String> backendCrews = FileReader.read("backend-crew.md");
-		List<String> frontedCrews = FileReader.read("frontend-crew.md");
+		while (programFlag) {
+			Print.options();
+			String input = validInput(Console.readLine());
+			if (input.equals(QUIT)) {
+				programFlag = false;
+			}
+			matching.selectOption(input);
+		}
+	}
 
+	private static String validInput(String input) {
+		try {
+			catchError(input);
+		} catch (IllegalArgumentException e) {
+			System.out.println("[ERROR] " + CONTROL_LIST + " 중에 입력하세요.");
+		}
+		return input;
+	}
+
+	private static void catchError(String input) {
+		if (!CONTROL_LIST.contains(input)) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
--- src/main/java/pairmatching/Matching.java
+++ src/main/java/pairmatching/Matching.java
@@ -1,2 +1,37 @@
-package pairmatching;public class Matching {
+package pairmatching;
+
+import static pairmatching.Options.*;
+
+import java.util.List;
+
+public class Matching {
+
+	private List<String> backend;
+	private List<String> frontend;
+
+	public Matching(List<String> backendCrews, List<String> frontedCrews) {
+		this.backend = backendCrews;
+		this.frontend = frontedCrews;
+	}
+
+	public void selectOption(String input) {
+		if (input.equals(MATCHING)) {
+			match();
+		}
+		if (input.equals(SELECT)) {
+			select();
+		}
+		if (input.equals(RESET)) {
+			reset();
+		}
+	}
+
+	public void match() {
+	}
+
+	public void select() {
+	}
+
+	public void reset() {
+	}
 }
