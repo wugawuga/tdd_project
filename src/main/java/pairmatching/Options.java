d809efa199c985adc62a5ad7cbc8152e74da5ece
wugawuga <dnr3655@naver.com>
2022-10-24 16:40:56 +0900

    feat: 옵션 상수화

---
+++ src/main/java/pairmatching/Options.java
@@ -0,0 +1,12 @@
+package pairmatching;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Options {
+	public static final String MATCHING = "1";
+	public static final String SELECT = "2";
+	public static final String RESET = "3";
+	public static final String QUIT = "Q";
+	public static final List<String> CONTROL_LIST = Arrays.asList(MATCHING, SELECT, RESET, QUIT);
+}
