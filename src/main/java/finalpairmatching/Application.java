127072abcabc1ce0dabd5e3cb6143afc01206612
wugawuga <dnr3655@naver.com>
2022-12-04 13:14:41 +0900

    feat: 크루 파일 입출력

--- src/main/java/finalpairmatching/Application.java
+++ src/main/java/finalpairmatching/Application.java
@@ -1,7 +1,42 @@
 package finalpairmatching;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Application {
+    private static final List<String> fileNames = Arrays.asList("backend-crew.md", "frontend-crew.md");
+
     public static void main(String[] args) {
-        // TODO 구현 진행
+        Map<String, List<String>> map = new HashMap<>();
+        for (String fileName : fileNames) {
+            String branch = divergeFileName(fileName);
+            List<String> crews = readFile(fileName);
+            map.put(branch, crews);
+        }
+    }
+
+    private static List<String> readFile(String fileName) {
+        try (InputStream is = Application.class.getClassLoader().getResource(fileName).openStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            return br.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 파일이름을 확인해주세요.");
+        }
+    }
+
+    private static String divergeFileName(String fileName) {
+        if (fileName.contains("backend-crew")) {
+            return "back";
+        }
+        if (fileName.contains("frontend-crew")) {
+            return "front";
+        }
+        throw new IllegalArgumentException("[ERROR] 파일이름은 'backend-crew', 'frontend-crew' 만 가능해요");
     }
 }
